5276f25a8d6903e29c698c3432a2c8e2343a8896
Jun <jun55tsuno@gmail.com>
2023-02-10 18:43:46 -0800

    updated

--- src/app/[todoId]/page.tsx
+++ src/app/[todoId]/page.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import axios from "axios";
+import { Todo } from "@/types/types";
 
-const TodoItem = () => {
-	return <div>TodoItem</div>;
+type PageProps = {
+	params: {
+		todoId: string;
+	};
 };
 
-export default TodoItem;
+const fetchTodo = async (todoId: string) => {
+	const res = await axios(
+		`https://jsonplaceholder.typicode.com/todos/${todoId}`
+	);
+	const todo: Todo = await res.data;
+	return todo;
+};
+
+const TodoPage = async ({ params }: PageProps) => {
+	const todo = await fetchTodo(params.todoId);
+
+	return (
+		<div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+			<p>
+				#{todo.id}: {todo.title}
+			</p>
+			<p>Completed: {todo.completed ? "Yes" : "No"}</p>
+			<p className="border-t border-black mt-5 text-right">
+				By User: {todo.userId}
+			</p>
+		</div>
+	);
+};
+
+export default TodoPage;
+
+//　ページが作られる前に、ビルドの段階でパスを作っておく。
+export async function generateStaticParams() {
+	const res = await axios("https://jsonplaceholder.typicode.com/todos/");
+	const todos: Todo[] = await res.data;
+
+	// Prebuilding only the first 10 pages to avoid being rate
+	const trimmedTodos = todos.splice(0, 5);
+
+	// return を忘れない！！[todoId]（フォルダ＝パス）がstringの為、stringにする
+	return trimmedTodos.map((todo) => {
+		return { todoId: todo.id.toString() };
+	});
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
+import axios from "axios";
+import { Todo } from "@/types/types";
+import Link from "next/link";
+
+const fetchTodos = async () => {
+	const res = await axios("https://jsonplaceholder.typicode.com/todos");
+	const todos: Todo[] = await res.data;
+	return todos;
+};
+
+const HomePage = async () => {
+	const todos = await fetchTodos();
+
+	let showContent;
+
+	if (!todos) {
+		showContent = <h1>No ToDo Items</h1>;
+	} else {
+		showContent = (
+			<ul>
+				{todos.length > 0 &&
+					todos.map((todo) => {
+						return (
+							<li key={todo.id}>
+								<Link href={`/${todo.id}`}>
+									{todo.title}, ID: {todo.id}
+								</Link>
+							</li>
+						);
+					})}
+			</ul>
+		);
+	}
+
 	return (
 		<>
-			<div>hello</div>
+			<div>{showContent}</div>
 		</>
 	);
-}
+};
+
+export default HomePage;
